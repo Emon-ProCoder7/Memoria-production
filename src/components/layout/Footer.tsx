d08e6b9ea3f09219675cca748d458f33984bc86e
Md Tabassum Hossain Emon <60403153+Emon-ProCoder7@users.noreply.github.com>
2025-12-22 09:35:40 -0600

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -41,7 +41,7 @@ export function Footer() {
             <ul className="space-y-4 text-muted-foreground">
               <li className="flex items-center gap-3">
                 <MapPin className="w-4 h-4 text-primary" />
-                <span>123 Creative Studio, LA</span>
+                <span>1209 MOUNTAIN ROAD   PL NE STE R,   ALBUQUERQUE, NM 87110</span>
               </li>
               <li className="flex items-center gap-3">
                 <Phone className="w-4 h-4 text-primary" />
@@ -77,6 +77,7 @@ export function Footer() {
     </footer>
   );
 }
+
 
 
 
