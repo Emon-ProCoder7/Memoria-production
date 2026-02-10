0e202b8f1c63403b57c8564b3e08c1341c30179e
Md Tabassum Hossain Emon <60403153+Emon-ProCoder7@users.noreply.github.com>
2026-02-10 13:54:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import LandingPage from '@/components/LandingPage';
 import { Toaster } from '@/components/ui/toaster';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from '@/components/LandingPage';
+import PrivacyPolicy from '@/components/PrivacyPolicy';
+import TermsAndConditions from '@/components/TermsAndConditions';
+import { Toaster } from '@/components/ui/toaster';
+
 function App() {
   return (
-    <>
-      <LandingPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+      </Routes>
       <Toaster />
-    </>
+    </BrowserRouter>
   );
 }
 
