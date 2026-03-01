063fa32a63dfa3f1b195841dc76a6ab06427f4c4
Md Tabassum Hossain Emon <60403153+Emon-ProCoder7@users.noreply.github.com>
2026-03-01 07:44:24 +0600

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -40,7 +40,7 @@ export function Footer() {
             <ul className="space-y-4 text-muted-foreground">
               <li className="flex items-center gap-3">
                 <MapPin className="w-4 h-4 text-primary" />
-                <span>1209 Mountain Road PL NE STE R, Albuquerque</span>
+                <span>1209 Mountain Road PL NE STE R, Albuquerque, NM 87110</span>
               </li>
               <li className="flex items-center gap-3">
                 <Phone className="w-4 h-4 text-primary" />
